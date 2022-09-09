67b0d47694027d2cf02f13cadf1252ecce2e92e4
Abraham Olamide James <abraham.o.james442@gmail.com>
2022-09-09 00:40:14 -0700

    4 put done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * * main - This function is the starting point
+ * * of the program that print a text to screen
+ * * Return: 0
+ * **/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
