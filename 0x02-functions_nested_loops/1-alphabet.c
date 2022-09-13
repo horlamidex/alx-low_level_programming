2ab34d108db9ada5dd8d36493d152d1fafb0c59f
Abraham Olamide James <abraham.o.james442@gmail.com>
2022-09-13 14:23:28 -0700

    Fixed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  * print_alphabet - a function that print all alphabets in lower case
+ *   * Return: void
+ *    **/
+void print_alphabet(void)
+{
+int alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+_putchar(alpha);
+_putchar('\n');
+}
+
