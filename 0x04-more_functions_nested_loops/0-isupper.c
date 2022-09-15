f7bcbd4b8b011600e0784d6ee97d361a56f47061
Abraham Olamide James <abraham.o.james442@gmail.com>
2022-09-15 04:24:38 -0700

    Fixed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* _isupper - check if input is uppercace.
-* @c: character to check
-* Return: Always 1 if yet and 0 if not.
-**/
+ * _isupper - check if input is captial letter.
+ * @c: input parameter
+ * Return: Always 0.
+ */
 int _isupper(int c)
 {
-int i;
-for (i = 65; i < 90; i++)
-{
-if (i == c)
-{
+if (c >= 'A' && c <= 'Z')
 return (1);
-}
-
-}
 return (0);
 }
 
