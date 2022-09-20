bbee0c676f6117e3d020dc09bfd48e16d35b2fc0
Abraham Olamide James <abraham.o.james442@gmail.com>
2022-09-20 09:41:09 -0700

    Done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that returns the length of a string
+ * @s: char parameter
+ * Return: integer length of a string
+ **/
+int _strlen(char *s)
+{
+return (strlen(s));
+}
+
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _puts -A Function that prints a string followd by a new line to stdout.
+ * @str: string parameter
+ * Return: void
+ **/
+void _puts(char *str)
+{
+puts(str);
+}
+
